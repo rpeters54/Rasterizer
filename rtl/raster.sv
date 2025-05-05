c8a008e28f84a3105c82568f005168568dbf0304
rpeter54 <rileyap02@gmail.com>
2025-05-05 16:00:17 -0700

    Cleaned up a lot

--- rtl/raster.sv
+++ rtl/raster.sv
@@ -2,32 +2,24 @@
 `define TILE_WIDTH   32
 `define TILE_COLUMNS 20
 `define TILE_ROWS    15
+`define NUM_VERTICES  3
 
-typedef union packed {
-  logic [27:0] raw;
-  struct packed {
+typedef struct packed {
     logic [9:0] x;    // 10-bit x component
     logic [9:0] y;    // 10-bit y component
     logic [7:0] z;    // 8-bit z component
-  } fields;
 } coord_3d_t;
 
-typedef union packed {
-  logic [19:0] raw;
-  struct packed {
+typedef struct packed {
     logic [9:0] x;    // 10-bit x component
     logic [9:0] y;    // 10-bit y component
-  } fields;
 } coord_2d_t;
 
-typedef union packed {
-  logic [15:0] raw;
-  struct packed {
+typedef struct packed {
     logic [3:0]  color;     // 4-bit color (bits 15:12)
     logic [2:0]  padding;   // 3-bit padding (bits 11:9)
     logic [3:0]  tile_y;  
     logic [4:0]  tile_x;
-  } fields;
 } polygon_t;
 
 module raster(
@@ -46,38 +38,47 @@ module raster(
     output coord_2d_t   pixel_out
 );
 
-logic [$clog2(`TILE_WIDTH*`TILE_WIDTH)-1:0] clk_accum = '0;
-always_ff @(posedge clk) begin
-    clk_accum <= clk_accum + 1;
-end
+// Treat v as an array of vertices
+assign v = '{v0, v1, v2};
 
-// used to track previous value in fib calc
-coord_2d_t   abs_pos, d_0, d_1, d_2;
+// valid signal set once tile is read from input
 logic        vld_0;
-logic [$clog2(`TILE_WIDTH*`TILE_WIDTH)*2-1:0] edge_0, edge_1, edge_2, rel_pos;
+
+// absolute position of the pixel in the tile
+coord_2d_t   abs_pos;
+
+// relative position of the pixel in the tile
+logic [$clog2(`TILE_WIDTH*`TILE_WIDTH)-1:0] rel_pos;
+
+// delta values used to update the edge functions
+coord_2d_t   d [0:`NUM_VERTICES-1];
+
+// current edge values
+logic [$clog2(`TILE_WIDTH*`TILE_WIDTH)*2-1:0] edges [0:`NUM_VERTICES-1];
+
+// color of the polygon
 logic [3:0]  color;
 
 logic [7:0] z_buffer     [0:(`TILE_WIDTH*`TILE_WIDTH)-1];
 logic [3:0] color_buffer [0:(`TILE_WIDTH*`TILE_WIDTH)-1];
-[$clog2(`TILE_WIDTH*`TILE_WIDTH)-1:0] rel_pos_1d;
 
 
 coord_2d_t temp_start;
-coord_2d_t temp_d_0, temp_d_1, temp_d_2;
+coord_2d_t temp_d [0:`NUM_VERTICES-1];
 always_comb begin
 
     // Shift is used to convert from tile coordinates to pixel coordinates
-    temp_start.fields.x = {metadata.fields.tile_x, 5'd0};
-    temp_start.fields.y = {1'd0, metadata.fields.tile_y, 5'd0};
+    temp_start.x = {metadata.tile_x, 5'd0};
+    temp_start.y = {1'd0, metadata.tile_y, 5'd0};
 
     // Compute the differences
-    temp_d_0.fields.x = v1.fields.x - v0.fields.x;
-    temp_d_0.fields.y = v1.fields.y - v0.fields.y;
-    temp_d_1.fields.x = v2.fields.x - v1.fields.x;
-    temp_d_1.fields.y = v2.fields.y - v1.fields.y;
-    temp_d_2.fields.x = v0.fields.x - v2.fields.x;
-    temp_d_2.fields.y = v0.fields.y - v2.fields.y;
+    deltas(temp_d, {v[1:2], v[0]}, v);
+end
 
+// Clock divider for multi-cycle path
+logic [$clog2(`TILE_WIDTH*`TILE_WIDTH)-1:0] clk_accum = '0;
+always_ff @(posedge clk) begin
+    clk_accum <= clk_accum + 1;
 end
 
 // load in the vertices and compute the edges
@@ -85,12 +86,13 @@ always_ff @(posedge clk) begin
 
     if (!rst_n) begin
         // Reset logic
-        current.raw = '0;
-        rel_pos_1d = '0;
-        d_0.raw = '0;
-        d_1.raw = '0;
-        d_2.raw = '0;
-        vld_0 = '0;
+        current = '{ default: '0 };
+        d       = '{ default: '0 };
+        edges   = '{ default: '0 };
+        abs_pos = '{ default: '0 };
+        rel_pos = '0;
+        vld_0   = '0;
+        vld_1   = '0;
         
     end else if (clk_accum == 0) begin  
         /* Only run this logic for each new tile */
@@ -99,27 +101,20 @@ always_ff @(posedge clk) begin
         vld_0 <= vld_in;
 
         // Store the color
-        color <= metadata.fields.color;
+        color <= metadata.color;
         
         // Store the start position for the next cycle
         abs_pos <= temp_start;
-        rel_pos_1d <= '0;
+        rel_pos <= '0;
 
         // Store the differences for the next cycle
-        d_0 <= temp_d_0;
-        d_1 <= temp_d_1;
-        d_2 <= temp_d_2;
+        for (int i = 0; i < `NUM_VERTICES; i++) begin
+            d[i] <= temp_d[i];
+        end
 
         // Compute the edges using the current values
         // Note: We use the immediate value of temp_start instead of the not-yet-updated start
-        edge_0 <= (temp_start.fields.x - v0.fields.x) * temp_d_0.fields.y +
-                  (temp_start.fields.y - v0.fields.y) * temp_d_0.fields.x;
-                  
-        edge_1 <= (temp_start.fields.x - v1.fields.x) * temp_d_1.fields.y +
-                  (temp_start.fields.y - v1.fields.y) * temp_d_1.fields.x;
-                  
-        edge_2 <= (temp_start.fields.x - v2.fields.x) * temp_d_2.fields.y +
-                  (temp_start.fields.y - v2.fields.y) * temp_d_2.fields.x;
+        edge_compute_base(edges, temp_start, temp_d, v);
 
         // Compute the z-depth value
         // Note: This is a placeholder and should be replaced with the actual z-depth calculation
@@ -132,55 +127,45 @@ end
 always_ff @(posedge clk) begin
 
     if (!rst_n == 0) begin
-        // Reset logic
-        abs_pos.raw = '0;
-        edge_0.raw = '0;
-        edge_1.raw = '0;
-        edge_2.raw = '0;
-        vld_1 = '0;
-        
+        // Reset should freeze this logic
     end else begin
         // edge and depth check
         if (vld_0 && $signed(edge_0) > 0 && $signed(edge_1) > 0 && $signed(edge_2) > 0 /* && z-depth check */) begin
             // Update the z-buffer
             /* ... */
             // Write to the color buffer
-            color_buffer[rel_pos_1d] <= color;
+            color_buffer[rel_pos] <= color;
         end 
 
         // Update next position and the edge values
-        if ((abs_pos.fields.y & `TILE_WIDTH-1) == (`TILE_WIDTH-1) 
-         && (abs_pos.fields.x & `TILE_WIDTH-1) == (`TILE_WIDTH-1)) begin
+        if ((abs_pos.y & `TILE_WIDTH-1) == (`TILE_WIDTH-1) 
+         && (abs_pos.x & `TILE_WIDTH-1) == (`TILE_WIDTH-1)) begin
 
             // At the end of a tile, do nothing until tile is updated
 
-        end else if ((abs_pos.fields.x & `TILE_WIDTH-1) == (`TILE_WIDTH-1)) begin
+        end else if ((abs_pos.x & `TILE_WIDTH-1) == (`TILE_WIDTH-1)) begin
             // Update the absolute position
-            abs_pos.fields.x <= abs_pos.fields.x - (`TILE_WIDTH-1);
-            abs_pos.fields.y <= abs_pos.fields.y + 1;
+            abs_pos.x <= abs_pos.x - (`TILE_WIDTH-1);
+            abs_pos.y <= abs_pos.y + 1;
 
             // Update the relative position
-            rel_pos_1d <= rel_pos_1d + 1;
+            rel_pos <= rel_pos + 1;
 
             // Update the edge values
-            edge_0 <= edge_0 - {d_0.fields.y, 5'd0} - d_0.fields.x;
-            edge_1 <= edge_1 - {d_1.fields.y, 5'd0} - d_1.fields.x;
-            edge_2 <= edge_2 - {d_2.fields.y, 5'd0} - d_2.fields.x;
+            edge_row_step(edges, d);
 
             // Update the z-depth value
             // Note: This is a placeholder and should be replaced with the actual z-depth calculation
 
         end else begin
             // Update the absolute position
-            abs_pos.fields.x <= abs_pos.fields.x + 1;
+            abs_pos.x <= abs_pos.x + 1;
 
             // Update the relative position
-            rel_pos_1d <= rel_pos_1d + 1;
+            rel_pos <= rel_pos + 1;
 
             // Update the edge values
-            edge_0 <= edge_0 + d_0.fields.x;
-            edge_1 <= edge_1 + d_1.fields.x;
-            edge_2 <= edge_2 + d_2.fields.x;
+            edge_column_step(edges, d);
 
             // Update the z-depth value
             // Note: This is a placeholder and should be replaced with the actual z-depth calculation
@@ -194,6 +179,54 @@ always_ff @(posedge clk) begin
 
 end
 
+// Compute the deltas between two sets of vertices
+function void deltas(
+    coord_2d_t out [0:`NUM_VERTICES-1], 
+    coord_3d_t a [0:`NUM_VERTICES-1], 
+    coord_3d_t b [0:`NUM_VERTICES-1]
+    );
+
+    for (int i = 0; i < `NUM_VERTICES; i++) begin
+        out[i].x = a[i].x - b[i].x;
+        out[i].y = a[i].y - b[i].y;
+    end
+
+endfunction
+
+// Compute the edge functions from the start position
+function void edge_compute_base(
+    logic [$clog2(`TILE_WIDTH*`TILE_WIDTH)*2-1:0] edges [0:`NUM_VERTICES-1], 
+    coord_2d_t start,
+    coord_2d_t d [0:`NUM_VERTICES-1],
+    coord_3d_t v [0:`NUM_VERTICES-1]
+    );
+    for (int i = 0; i < `NUM_VERTICES; i++) begin
+        edges[i] <= (start.x - v[i].x) * d[i].y +
+                    (start.y - v[i].y) * d[i].x;
+    end
+endfunction
+
+// Update the edge functions for a step in the column direction
+function void edge_column_step(
+    logic [$clog2(`TILE_WIDTH*`TILE_WIDTH)*2-1:0] edges [0:`NUM_VERTICES-1], 
+    coord_2d_t d [0:`NUM_VERTICES-1]
+    );
+
+    for (int i = 0; i < `NUM_VERTICES; i++) begin
+        edges[i] <= edges[i] + d[i];
+    end
+endfunction
+
+// Update the edge functions for a step in the row direction
+function void edge_row_step(
+    logic [$clog2(`TILE_WIDTH*`TILE_WIDTH)*2-1:0] edges [0:`NUM_VERTICES-1], 
+    coord_2d_t d [0:`NUM_VERTICES-1]
+    );
+
+    for (int i = 0; i < `NUM_VERTICES; i++) begin
+        edges[i] <= edges[i] - {d[i].y, 5'd0} - d[i].x;
+    end
+endfunction
 
 
 endmodule
