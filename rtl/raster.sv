c0399629aae6bd58c5157756971fe0bc5ebaf527
rpeter54 <rileyap02@gmail.com>
2025-05-07 15:30:17 -0700

    Translating to State Machine to make it easier to understand

--- rtl/raster.sv
+++ rtl/raster.sv
@@ -34,17 +34,31 @@ typedef struct packed {
     logic [4:0]  tile_x;
 } metadata_t;
 
+typedef enum {
+    INPUT,
+    DETERMINANT,
+    SCALING,
+    Z_VALUE,
+    PASS_ONWARD
+} tile_state_t;
+
+typedef enum {
+    IDLE,
+    FLUSHING,
+    COMPUTING
+} pixel_state_t;
+
 module raster(
     input             clk,
     input             rst_n,
-    input             ready_in,
+    input             rdy_out,
     input             vld_in,
     input  coord_3d_t v0,
     input  coord_3d_t v1,
     input  coord_3d_t v2,
     input  metadata_t metadata,
 
-    output logic        rdy_out,
+    output logic        rdy_in,
     output logic        vld_out,
     output logic [`NUM_COLORS-1:0]  color_out,
     output coord_2d_t   pixel_out
@@ -52,16 +66,17 @@ module raster(
 
 // Treat v as an array of vertices
 coord_3d_t v [0:`NUM_VERTICES-1];
-assign v = '{v0, v1, v2};
+v = '{v0, v1, v2};
 
-// valid signal set once tile is read from input
-logic  vld[0:`PIPELINE_STAGES-1];
+tile_state_t tile_present_state, tile_next_state;
+tile_present_state = INPUT;
 
 logic signed [`FX_TOTAL_BITS*2-1:0] z_buffer     [0:`TILE_AREA-1];
 logic [3:0]                         color_buffer [0:`TILE_AREA-1];
 
 coord_3d_t temp_start;
 coord_3d_t temp_d [0:`NUM_VERTICES-1];
+
 always_comb begin
     // convert from tile number to pixel coordinates
     tile_to_coord(temp_start, metadata);
@@ -69,6 +84,36 @@ always_comb begin
     deltas(temp_d, {v[1:2], v[0]}, v);
 end
 
+always_comb begin
+    case (tile_present_state)
+        INPUT: begin
+            if (vld_in && rdy_in) begin
+                tile_next_state = DETERMINANT;
+            end else begin
+                tile_next_state = INPUT;
+            end
+        end
+        DETERMINANT: begin
+            tile_next_state = SCALING;
+        end
+        SCALING: begin
+            tile_next_state = Z_VALUE;
+        end
+        Z_VALUE: begin
+            tile_next_state = PASS_ONWARD;
+        end
+        PASS_ONWARD: begin
+            if (rdy_out) begin
+                tile_next_state = INPUT;
+            end else begin
+                tile_next_state = PASS_ONWARD;
+            end
+        end
+        default: begin
+            tile_next_state = INPUT;
+        end
+    endcase
+end
 
 coord_3d_t                          abs_pos;
 logic        [`TILE_BIT_WIDTH-1:0]  rel_pos;
@@ -76,8 +121,10 @@ coord_3d_t                          deltas  [0:`NUM_VERTICES-1];
 logic signed [`FX_TOTAL_BITS*2-1:0] edges   [0:`NUM_VERTICES-1];
 logic [3:0]                         color;
 
-logic                               lock;
-// Cycle 1: Calculate differences and edges (no division)
+logic [`FX_TOTAL_BITS*2-1:0] tri_area_2, dzdx_undiv, dzdy_undiv;  
+logic [`FX_TOTAL_BITS*2-1:0] dzdx, dzdy;
+logic [`FX_TOTAL_BITS*2-1:0] current_z;
+
 always_ff @(posedge clk) begin 
     if (!rst_n) begin
         // Reset logic
@@ -89,79 +136,69 @@ always_ff @(posedge clk) begin
         color           <= '0;
         z_buffer        <= '{ default: '0 };
         color_buffer    <= '{ default: '0 };
-        lock            <= '0;
-    end else begin
-        vld[0] <= lock ? '0 : vld_in;
 
-        if (vld_in) begin
-            lock <= '1;
-        end
-        if (vld_in & !lock) begin
-            color <= metadata.color;
-            
-            // Store the start position for the next cycle
-            abs_pos <= temp_start;
-            rel_pos <= '0;
-
-            // Store the differences for the next cycle
-            for (int i = 0; i < `NUM_VERTICES; i++) begin
-                deltas[i] <= temp_d[i];
-            end
-
-            // Compute the edges using the current tile's top-left pixel
-            for (int i = 0; i < `NUM_VERTICES; i++) begin
-                edges[i] <= (temp_start.x - v[i].x) * temp_d[i].y +
-                            (temp_start.y - v[i].y) * temp_d[i].x;
-            end
-        end
-    end
-end
-
-
-logic [`FX_TOTAL_BITS*2-1:0] tri_area_2,dzdx_undiv,dzdy_undiv;  
-always_ff @(posedge clk) begin 
-    if (!rst_n) begin
-        vld[1]     <= '0;
         tri_area_2 <= '0;
         dzdx_undiv <= '0;
         dzdy_undiv <= '0;
-    end else begin
-        // Propagate the valid signal
-        vld[1] <= vld[0];
 
-        // Compute the determinant, and unnormalized dz
-        tri_area_2  <= (deltas[0].x * deltas[2].y - deltas[2].x * deltas[0].y);
-        dzdx_undiv  <= (deltas[1].y * v[0].z + deltas[2].y * v[1].z + deltas[0].y * v[2].z);
-        dzdy_undiv  <= -(deltas[0].x * v[0].z + deltas[1].x * v[1].z + deltas[2].x * v[2].z);
+        dzdx <= '0;
+        dzdy <= '0;
+
+        current_z <= '0;
+
+        rdy_in          <= '1;
+    end else begin
+        present_state <= next_state;
+
+        case (present_state)
+            INPUT: begin
+                // Deassert ready signal if valid input is received
+                rdy_in <= vld_in ? '0 : rdy_in;
+
+                // Store the color
+                color <= metadata.color;
+            
+                // Store the start position for the next cycle
+                abs_pos <= temp_start;
+                rel_pos <= '0;
+
+                // Store the differences for the next cycle
+                for (int i = 0; i < `NUM_VERTICES; i++) begin
+                    deltas[i] <= temp_d[i];
+                end
+
+                // Compute the edges using the current tile's top-left pixel
+                for (int i = 0; i < `NUM_VERTICES; i++) begin
+                    edges[i] <= (temp_start.x - v[i].x) * temp_d[i].y +
+                                (temp_start.y - v[i].y) * temp_d[i].x;
+                end
+            end
+            DETERMINANT: begin
+                // Compute the determinant of the triangle and unscaled dzdx and dzdy
+                tri_area_2  <= (deltas[0].x * deltas[2].y - deltas[2].x * deltas[0].y);
+                dzdx_undiv  <= (deltas[1].y * v[0].z + deltas[2].y * v[1].z + deltas[0].y * v[2].z);
+                dzdy_undiv  <= -(deltas[0].x * v[0].z + deltas[1].x * v[1].z + deltas[2].x * v[2].z);
+            end
+            SCALING: begin
+                // Compute the scaled dzdx and dzdy
+                dzdx    <= (dzdx_undiv/tri_area_2);
+                dzdy    <= (dzdy_undiv/tri_area_2);
+            end
+            Z_VALUE: begin
+                // Compute the z for the top left pixel
+                current_z <= v[0].z + (v[0].x - abs_pos.x) * dzdx + (v[0].y - abs_pos.y) * dzdy
+            end
+            PASS_ONWARD: begin
+                // Propagate the valid signal
+                vld[0] <= '1;
+            end
+        endcase
     end
 end
 
-logic [`FX_TOTAL_BITS*2-1:0] dzdx,dzdy;
-always_ff @(posedge clk) begin 
-    if (!rst_n) begin
-        vld[2]      <= '0;
-    end else begin
-        // Propagate the valid signal
-        vld[2]  <= vld[1];
 
-        // normalize dz
-        dzdx    <= (dzdx_undiv/tri_area_2);
-        dzdy    <= (dzdy_undiv/tri_area_2);
-    end
-end
-
-logic [`FX_TOTAL_BITS*2-1:0] current_z;
-always_ff @(posedge clk) begin
-    if (!rst_n) begin
-        vld[3]  <= '0;
-    end else begin
-        // Propagate the valid signal
-        vld[3]  <= vld[2];
-
-        // calculate z-value of the top-left pixel in the tile
-        current_z <= v[0].z + (v[0].x - abs_pos.x) * dzdx + (v[0].y - abs_pos.y) * dzdy
-    end
-end
+pixel_state_t pixel_present_state, pixel_next_state;
+pixel_present_state = IDLE;
 
 // Process each pixel in the tile
 always_ff @(posedge clk) begin
@@ -227,7 +264,7 @@ function logic inside_polygon(
 endfunction
 
 function logic depth_test(
-    logic [`TILE_BIT_WIDTH-1:0] rel_pos,
+    logic        [`TILE_BIT_WIDTH-1:0]  rel_pos,
     logic signed [`FX_TOTAL_BITS*2-1:0] z_current,
     logic signed [`FX_TOTAL_BITS*2-1:0] z_buffer  [0:`TILE_AREA-1];
     );
